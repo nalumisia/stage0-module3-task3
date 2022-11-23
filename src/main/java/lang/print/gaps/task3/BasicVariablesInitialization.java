ea1aec094d60f77d92de47bf41208ce288891d70
nalumisia <paulincia@mailplus.pl>
2022-11-23 19:51:23 +0100

    solution

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -3,5 +3,12 @@ package lang.print.gaps.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int one = 1;
+        int two = 10;
+        int three = 100;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
     }
 }
